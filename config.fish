ace25afb177ee64d0e7b67e939ec9455b2d0f8ce
agent_denton <andybrychenko@gmail.com>
2021-07-08 23:21:24 +0300

    add fzf layout

--- config.fish
+++ config.fish
@@ -4,6 +4,9 @@ set -x LANG en_US.UTF-8
 set -x EDITOR nvim
 set -x VISUAL nvim
 set -x MANPAGER "nvim -c 'set ft=man' -"
+set -x FZF_DEFAULT_OPTS "-m --height 50% --border"
+set -x FZF_DEFAULT_OPTS "--color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C" $FZF_DEFAULT_OPTS
+set -x FZF_DEFAULT_OPTS "--color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B" $FZF_DEFAULT_OPTS
 
 alias lg="lazygit"
 alias ls="exa"
@@ -21,7 +24,6 @@ alias njb="clear && ninja -C build"
 alias mcb="meson . build --cross-file=cross.ini"
 alias mcbd="meson . build --cross-file=cross.ini --buildtype=debug"
 alias mcbr="meson . build --cross-file=cross.ini --buildtype=release"
-alias oni="/home/agent_denton/github/oni2/_esy/release/install/bin/Oni2 & disown"
 
 fish_vi_key_bindings
 set fish_greeting
