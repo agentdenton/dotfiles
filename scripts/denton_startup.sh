0ceee4c1ae6d479c3afee23cd1645ea6ccf99fd7
agent_denton <agentdenton3@gmail.com>
2021-10-07 23:57:39 +0300

    properly start apps in autostart script

--- scripts/denton_startup.sh
+++ scripts/denton_startup.sh
@@ -6,7 +6,17 @@ setxkbmap -model pc105 -layout us,ua -variant qwerty -option grp:win_space_toggl
 xset r rate 400 100
 # xrandr --output DVI-D-1 --primary --left-of HDMI-0
 
-dunst &
-flameshot &
-xss-lock --transfer-sleep-lock -- i3lock -c 000000 --nofork &
-feh --bg-scale $HOME/Pictures/wallpapers/rengoku_cool.jpg
+function start_apps {
+    # close all open apps
+    apps=(dunst flameshot xss-lock)
+    for app in ${apps[@]}; do
+        killall $app
+    done
+
+    dunst &
+    flameshot &
+    xss-lock --transfer-sleep-lock -- i3lock -i ~/pictures/lock_screen/space_man.png --nofork &
+    feh --bg-scale $HOME/pictures/wallpapers/space_man.jpg
+}
+
+start_apps
