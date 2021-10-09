4ee65f216649646743d1dac69f115be87052f288
agent_denton <agentdenton3@gmail.com>
2021-10-09 20:20:21 +0300

    remove unneccessary plugins, clean up the configs in neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,80 @@
 require('settings')
 require('mappings')
-require('plugins')
+
+local api = vim.api
+local fn = vim.fn
+local cmd = vim.md
+local g = vim.g
+local execute = vim.api.nvim_command
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
+        'git',
+        'clone',
+        'https://github.com/wbthomason/packer.nvim',
+        install_path
+    })
+    execute 'packadd packer.nvim'
+end
+
+require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+    use 'tpope/vim-commentary'
+    use 'Yggdroot/indentLine'
+    use "projekt0n/github-nvim-theme"
+    use 'hoob3rt/lualine.nvim'
+    use 'norcalli/nvim-colorizer.lua'
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+    }
+end)
+
+require('colorizer').setup()
+
+require('nvim-treesitter.configs').setup({
+    highlight = {
+        enable = true,
+    },
+    ensure_installed = {
+        'c',
+        'bash',
+        'lua',
+        'python',
+        'rust',
+        'julia',
+    },
+})
+
+require('lualine').setup({
+    options = {
+        icons_enabled = true,
+        theme = 'github',
+        component_separators = { ' ', ' ' },
+        section_separators = { '', '' },
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = {
+            {
+                'diagnostics',
+                sources = { 'nvim_lsp' },
+                symbols = {
+                    hint = '  ',
+                },
+                sections = { 'error', 'warn', 'info', 'hint' },
+            },
+            'progress',
+        },
+        lualine_y = { 'filetype' },
+        lualine_z = { 'location' }
+    }
+})
 
 require("github-theme").setup({
     theme_style = "dark",
