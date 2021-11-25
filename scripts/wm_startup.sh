218117929ddf0273d9bfc79db935891ad6a543de
agent_denton <agentdenton3@gmail.com>
2021-11-25 13:29:59 +0200

    i3: new keyboard tweaks

--- scripts/wm_startup.sh
+++ scripts/wm_startup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-setxkbmap -option caps:swapescape
-setxkbmap -model pc105 -layout us,ua -variant qwerty -option grp:win_space_toggle
-
 xset r rate 400 100
+setxkbmap -model pc105 -layout us,ua -variant qwerty -option grp:win_space_toggle
 # xrandr --output DVI-D-1 --primary --left-of HDMI-0
 
 # close all open apps
-apps=(dunst flameshot xss-lock)
+apps=(dunst flameshot xss-lock polybar feh)
 for app in ${apps[@]}; do
     killall $app
 done
