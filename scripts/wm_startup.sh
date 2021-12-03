1eeb74b5da1b78c817cde1519af46185761670a1
agent_denton <agentdenton3@gmail.com>
2021-12-03 19:14:45 +0200

    scripts: better wm_startup.sh

--- scripts/wm_startup.sh
+++ scripts/wm_startup.sh
@@ -2,16 +2,18 @@
 
 xset r rate 400 100
 setxkbmap -model pc105 -layout us,ua -variant qwerty -option grp:win_space_toggle
-# xrandr --output DVI-D-1 --primary --left-of HDMI-0
 
-# close all open apps
-apps=(dunst flameshot xss-lock polybar feh)
-for app in ${apps[@]}; do
+# a dictionary of apps to start-restart with their input arguments
+declare -A apps=(
+    ["dunst"]=""
+    ["flameshot"]=""
+    ["polybar"]="top_bar"
+    ["xss-lock"]="--transfer-sleep-lock -- i3lock -i ~/pictures/lock_screen/retro_anime_girl.png --nofork"
+    ["feh"]="--bg-scale $HOME/pictures/wallpapers/retro_anime_girl.png"
+)
+
+# auto start apps with arguments, key = arg, vals[key] = app name
+for app in "${!apps[@]}"; do
     killall $app
+    $app "${apps[$app]}" &
 done
-
-dunst &
-flameshot &
-polybar top_bar &
-xss-lock --transfer-sleep-lock -- i3lock -i ~/pictures/lock_screen/retro_anime_girl.png --nofork &
-feh --bg-scale $HOME/pictures/wallpapers/retro_anime_girl.png
