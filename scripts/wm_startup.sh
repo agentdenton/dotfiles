cd4ab24a281c4e1d0d622a1a79a3b233ca192295
agent_denton <agentdenton3@gmail.com>
2021-12-05 16:36:59 +0200

    scripts: simplify startup script

--- scripts/wm_startup.sh
+++ scripts/wm_startup.sh
@@ -3,17 +3,19 @@
 xset r rate 400 100
 setxkbmap -model pc105 -layout us,ua -variant qwerty -option grp:win_space_toggle
 
-# a dictionary of apps to start-restart with their input arguments
-declare -A apps=(
-    ["dunst"]=""
-    ["flameshot"]=""
-    ["polybar"]="top_bar"
-    ["xss-lock"]="--transfer-sleep-lock -- i3lock -i ~/pictures/lock_screen/retro_anime_girl.png --nofork"
-    ["feh"]="--bg-scale $HOME/pictures/wallpapers/retro_anime_girl_gimped.png"
-)
+# array of apps to start
+cmds=()
+cmds+=("dunst")
+cmds+=("flameshot")
+cmds+=("polybar top_bar")
+cmds+=("xss-lock --transfer-sleep-lock -- i3lock -c 000000 --nofork")
+cmds+=("feh --bg-scale $HOME/pictures/wallpapers/retro_anime_girl_gimped.png")
 
 # auto start apps with arguments, key = arg, vals[key] = app name
-for app in "${!apps[@]}"; do
+for cmd in "${cmds[@]}"; do
+    # kill all apps that is running
+    app=$(echo $cmd | awk '{split($0,app," "); print app[1]}')
     killall $app
-    $app "${apps[$app]}" &
+    # execute app startup cmd
+    $cmd &
 done
