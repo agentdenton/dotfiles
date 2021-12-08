9c5abd53b348d63160e6fcde21a4c9200a740696
agent_denton <agentdenton3@gmail.com>
2021-12-08 21:02:33 +0200

    scripts: simpler startup script

--- scripts/wm_startup.sh
+++ scripts/wm_startup.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
+function start_app() { killall "$1"; eval "$1 $2" & }
+
 xset r rate 400 100
 setxkbmap -model pc105 -layout us,ua -variant qwerty -option grp:win_space_toggle
 
-# array of apps to start
-cmds=()
-cmds+=("dunst")
-cmds+=("flameshot")
-cmds+=("polybar top_bar")
-cmds+=("xss-lock --transfer-sleep-lock -- i3lock -c 000000 --nofork")
-cmds+=("feh --bg-scale $HOME/pictures/wallpapers/retro_anime_girl_gimped.png")
-
-# auto start apps with arguments, key = arg, vals[key] = app name
-for cmd in "${cmds[@]}"; do
-    # kill all apps that is running
-    app=$(echo $cmd | awk '{split($0,app," "); print app[1]}')
-    killall $app
-    # execute app startup cmd
-    $cmd &
-done
+start_app "dunst"
+start_app "flameshot"
+start_app "polybar" "top_bar"
+start_app "xss-lock" "--transfer-sleep-lock -- i3lock -i ~/pictures/wallpapers/onedark_triangles.png --nofork"
+start_app "feh" "--bg-scale ~/pictures/wallpapers/onedark_triangles.png"
