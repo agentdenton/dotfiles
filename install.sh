e16d7c29964e7d689f1e85c489b04d10e8e84ad6
agent_denton <agentdenton3@gmail.com>
2022-01-31 14:03:31 +0200

    add simplified install script

--- install.sh
+++ install.sh
@@ -9,3 +9,14 @@ if [[ $(pwd) != $DOTS_DIR ]]; then
     echo "Run this script from $DOTS_DIR"
     exit 1
 fi
+
+cp bashrc ~/.bashrc
+cp tmux.conf ~/.tmux.conf
+cp vimrc ~/.vimrc
+cp xinitrc ~/.xinitrc
+cp Xresources ~/.Xresources
+
+cp starship.toml ~/.config
+cp -r config/* ~/.config
+
+sudo cp -r scripts/* $INSTALL_DIR
