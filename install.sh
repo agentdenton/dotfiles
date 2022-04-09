54f0eadbe6577ee736db0c6e9d39dc41be7afd0e
Andy Brychenko <andybrychenko@gmail.com>
2022-04-09 17:31:50 +0300

    update install script

--- install.sh
+++ install.sh
@@ -2,21 +2,16 @@
 
 set -e
 
-INSTALL_DIR=/usr/local/bin
-DOTS_DIR=~/.dotfiles
-
-if [[ $(pwd) != $DOTS_DIR ]]; then
-    echo "Run this script from $DOTS_DIR"
+dots_dir=~/.dotfiles
+if [[ $(pwd) != $dots_dir ]]; then
+    echo "Run this script from $dots_dir"
     exit 1
 fi
 
 cp bashrc ~/.bashrc
 cp tmux.conf ~/.tmux.conf
 cp vimrc ~/.vimrc
+cp starship.toml ~/.config
 cp xinitrc ~/.xinitrc
 cp Xresources ~/.Xresources
-
-cp starship.toml ~/.config
 cp -r config/* ~/.config
-
-sudo cp -r scripts/* $INSTALL_DIR
