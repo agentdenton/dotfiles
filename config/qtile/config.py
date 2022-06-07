2398111b136a34002944938bdd750cf18030c34e
Andy Brychenko <andybrychenko@gmail.com>
2022-06-07 21:31:18 +0300

    qtile: add new layout, make config cleaner

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -5,9 +5,11 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
-DEFALT_MARGIN = 5
+DEFAULT_MARGIN = 5
 DEFAULT_MARGIN_ON_SINGLE = 5
-DEFALT_BORDER_WIDTH = 2
+DEFAULT_BORDER_WIDTH = 2
+
+MONADWIDE_RATIO = 0.70
 
 mod = "mod4"
 terminal = "alacritty"
@@ -53,27 +55,26 @@ keys = [
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod, "control"], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
+	Key([mod], "n", lazy.group.next_window(), desc="Move window focus to next window"),
+	Key([mod,  "shift"], "n", lazy.group.prev_window(), desc="Move window focus to previous window"),
+
+    Key([mod, "shift"], "c", lazy.window.kill(), desc="Close focused window"),
     Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
 
     Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen mode"),
-    Key([mod,  "shift"], "c", lazy.window.kill(), desc="Close focused window"),
+	Key([mod], "Tab", lazy.screen.toggle_group(), desc="Toggle between previous and current group"),
+	Key([mod, "shift"], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
 
 	Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-	Key([mod], "Tab", lazy.screen.toggle_group(), desc="Toggle between previous and current group"),
-	Key([mod,  "shift"], "e", lazy.spawn("qtile_exit.sh"), desc="Run the shutdown script"),
-	Key([mod], "n", lazy.layout.next(), desc="Move window focus to other window"),
-	Key([mod,  "shift"], "n", lazy.next_layout(), desc="Toggle between layouts"),
 	Key([mod], "d", lazy.spawn("rofi -show run"), desc="Launch app launcher"),
+	Key([mod,  "shift"], "e", lazy.spawn("qtile_exit.sh"), desc="Run the shutdown script"),
 	Key([mod], "Print", lazy.spawn("flameshot gui"), desc="Launch screenshot utility"),
 
 	# Audio control
     Key([mod], "F1", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle"), desc="Mute audio"),
     Key([mod], "F2", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%"), desc="Volume down"),
     Key([mod], "F3", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%"), desc="Volume up"),
-
-    # Lock the screen
-    Key([mod], "F9", lazy.spawn("i3lock -u -c 2e3440"), desc="Start the screenlocker"),
 
 	# TODO: brightness control
     # Key([mod], "F4", lazy.spawn("light -A 1"), desc="Volume up"),
@@ -104,13 +105,43 @@ column_layout_theme = {
     "border_focus_stack": "#88C0D0",
     "border_normal": "#3b4252",
     "border_normal_stack": "#3b4252",
-    "border_width": DEFALT_BORDER_WIDTH,
-    "margin": DEFALT_MARGIN,
+    "border_width": DEFAULT_BORDER_WIDTH,
+    "margin": DEFAULT_MARGIN,
     "margin_on_single": DEFAULT_MARGIN_ON_SINGLE,
+}
+
+monadwide_layout_theme = {
+    "ratio": MONADWIDE_RATIO,
+    "max_ratio": MONADWIDE_RATIO,
+    "border_focus": "#88C0D0",
+    "border_normal": "#3b4252",
+    "border_width": DEFAULT_BORDER_WIDTH,
+    "margin": DEFAULT_MARGIN,
+    "single_margin": DEFAULT_MARGIN_ON_SINGLE,
+    "single_border_width": DEFAULT_BORDER_WIDTH,
+}
+
+float_layout_theme = {
+    "border_focus": "#88C0D0",
+    "border_normal": "#3b4252",
+    "border_width": DEFAULT_BORDER_WIDTH,
 }
 
 layouts = [
     layout.Columns(**column_layout_theme),
+    layout.MonadWide(**monadwide_layout_theme),
+    layout.Floating(
+        **float_layout_theme,
+        float_rules=[
+                *layout.Floating.default_float_rules,
+                Match(wm_class="confirmreset"),
+                Match(wm_class="makebranch"),
+                Match(wm_class="maketag"),
+                Match(wm_class="ssh-askpass"),
+                Match(title="branchdialog"),
+                Match(title="pinentry"),
+            ]
+        )
 ]
 
 widget_defaults = dict(
@@ -148,11 +179,11 @@ screens = [
             ],
             30,
 			background="#2e3440",
-			margin=DEFALT_MARGIN,
+			margin=DEFAULT_MARGIN,
         ),
-		right=bar.Gap(DEFALT_MARGIN),
-		left=bar.Gap(DEFALT_MARGIN),
-		bottom=bar.Gap(DEFALT_MARGIN),
+		right=bar.Gap(DEFAULT_MARGIN),
+		left=bar.Gap(DEFAULT_MARGIN),
+		bottom=bar.Gap(DEFAULT_MARGIN),
     ),
 ]
 
@@ -162,24 +193,3 @@ mouse = [
     Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
-
-
-float_layout_theme = {
-    "border_focus": "#88C0D0",
-    "border_normal": "#3b4252",
-    "border_width": DEFALT_BORDER_WIDTH,
-}
-
-floating_layout = layout.Floating(
-    **float_layout_theme,
-    float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
-        *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
-        Match(title="pinentry"),  # GPG key password entry
-    ]
-)
