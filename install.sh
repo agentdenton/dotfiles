4ce828a68a6a19e4541755e42a925aa8c0bddfba
Andy Brychenko <andybrychenko@gmail.com>
2022-11-26 14:15:15 +0200

    update install script

--- install.sh
+++ install.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-dots_dir="~/.dotfiles"
-if [[ $(dirs) != $dots_dir ]]; then
-  echo "Run this script from $dots_dir"
-  exit 1
-fi
-
 ln -fns ~/.dotfiles/bashrc ~/.bashrc
 ln -fns ~/.dotfiles/starship.toml ~/.config/starship.toml
 ln -fns ~/.dotfiles/vimrc ~/.vimrc
