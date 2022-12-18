904dc5cabe09f658fc663ec2520b659389133d94
Andy Brychenko <andybrychenko@gmail.com>
2022-12-18 18:30:27 +0200

    update the install script

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 set -e
 
 ln -fns ~/.dotfiles/bashrc ~/.bashrc
-ln -fns ~/.dotfiles/starship.toml ~/.config/starship.toml
 ln -fns ~/.dotfiles/vimrc ~/.vimrc
 ln -fns ~/.dotfiles/nvim ~/.config
+ln -fns ~/.dotfiles/sway ~/.config
+ln -fns ~/.dotfiles/waybar ~/.config
+ln -fns ~/.dotfiles/rofi ~/.config
+ln -fns ~/.dotfiles/starship.toml ~/.config/starship.toml
+ln -fns ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+sudo cp scripts/* /usr/local/bin
+sudo cp services/* /etc/systemd/system/
