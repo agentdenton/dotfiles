ece0170c13a7d3b4772bd2afafe259fa301fac54
Andy Brychenko <andybrychenko@gmail.com>
2023-01-01 17:21:26 +0200

    scripts: update sway_brightness_keyboard.py

--- scripts/sway_brightness_keyboard.py
+++ scripts/sway_brightness_keyboard.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python
 
 import sys
+import logging
+
+MAX_BRIGHTNESS = 3
+MIN_BRIGHTNESS = 0
 
 def main():
-    fpath = '/sys/class/leds/asus::kbd_backlight/brightness'
-    with open(fpath, 'r+') as f:
-        curr_br = int(f.readline())
+    logging.basicConfig(level=logging.ERROR,
+                        format='%(levelname)s: %(message)s')
 
-        if len(sys.argv) > 1:
-            value = sys.argv[1]
-            print(f'argument value = {value}')
+    if len(sys.argv) < 2:
+        logging.error('No arguments provided')
+        sys.exit(1)
 
-            if value.startswith('+'):
-                next_br = curr_br + int(value.split('+')[1])
-            elif value.startswith('-'):
-                next_br = curr_br - int(value.split('-')[1])
-            else:
-                next_br = int(value)
+    br = sys.argv[1]
+    logging.info(f'arg brightness value = {br}')
 
-            print(f'current brightness = {curr_br}')
-            print(f'next brightness = {next_br}')
+    fdpath = '/sys/class/leds/asus::kbd_backlight/brightness'
+    with open(fdpath, 'r+') as fd:
+        cur_br = int(fd.readline())
+        logging.info(f'current brightness = {cur_br}')
+        if br.startswith('+'):
+            br = cur_br + int(br.split('+')[1])
+        elif br.startswith('-'):
+            br = cur_br - int(br.split('-')[1])
+        else:
+            br = int(br)
 
-            if next_br <= 3 and next_br >= 0:
-                f.write(str(next_br) + '\n')
+        if br > MAX_BRIGHTNESS:
+            br = MIN_BRIGHTNESS
+
+        if br < MIN_BRIGHTNESS:
+            br = MIN_BRIGHTNESS
+
+        fd.write(str(br) + '\n')
+
+    logging.info(f'Succes. Brightness changed to {br}')
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
