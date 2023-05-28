dd0740638c6fca0dd44ccbbafa928e5a1b8c0f2e
Andy Brychenko <andybrychenko@gmail.com>
2023-05-28 19:29:49 +0300

    replace the install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ln -fns ~/.dotfiles/bashrc ~/.bashrc
+ln -fns ~/.dotfiles/inputrc ~/.inputrc
+ln -fns ~/.dotfiles/vimrc ~/.vimrc
+ln -fns ~/.dotfiles/nvim ~/.config
+ln -fns ~/.dotfiles/sway ~/.config
+ln -fns ~/.dotfiles/waybar ~/.config
+ln -fns ~/.dotfiles/rofi ~/.config
+ln -fns ~/.dotfiles/ranger ~/.config
+ln -fns ~/.dotfiles/zathura ~/.config
+ln -fns ~/.dotfiles/zellij ~/.config
+ln -fns ~/.dotfiles/starship.toml ~/.config
+ln -fns ~/.dotfiles/alacritty ~/.config
