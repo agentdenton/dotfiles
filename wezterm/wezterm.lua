08eef213e52cfbf08f1af7f67593c5d406578da4
Andy Brychenko <andybrychenko@gmail.com>
2024-09-02 13:06:00 +0300

    wezterm: Updaet config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,93 +1,103 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local c = wezterm.config_builder()
 local act = wezterm.action
 
 local custom_catppuccin = wezterm.color.get_builtin_schemes()["Catppuccin Frappe"]
-custom_catppuccin.cursor_bg = '#B5BFE2'
+custom_catppuccin.cursor_bg = "#B5BFE2"
 
 c.color_schemes = {
-    ['CustomCatppuccin'] = custom_catppuccin,
+	["CustomCatppuccin"] = custom_catppuccin,
 }
 
-c.color_scheme = 'CustomCatppuccin'
+c.color_scheme = "CustomCatppuccin"
 
-c.font = wezterm.font({
-  family = 'ComicMono'
+c.font = wezterm.font_with_fallback({
+	{ family = "JetBrainsMono", weight = "Medium", italic = false },
 })
 
-c.font_size = 15
+c.front_end = "WebGpu"
+
+c.font_size = 14
 c.command_palette_font_size = c.font_size * 1.1
 
 c.use_fancy_tab_bar = false
 
-c.audible_bell = 'Disabled'
+c.audible_bell = "Disabled"
 
 c.adjust_window_size_when_changing_font_size = false
-c.window_padding = {left = 0, right = 0, top = 15, bottom = 0}
+c.window_padding = { left = 0, right = 0, top = 15, bottom = 0 }
 
-c.inactive_pane_hsb = {brightness = 0.90}
+c.inactive_pane_hsb = { brightness = 0.90 }
+
+-- ligatures
+c.harfbuzz_features = { "calt=0" }
 
 -- TODO: Explore https://github.com/nekowinston/dotfiles/blob/main/home/apps/wezterm/wezterm.lua
 c.keys = {
-  {
-    key = '\\',
-    mods = 'ALT',
-    action = act.SplitHorizontal({domain = "CurrentPaneDomain"}),
-  },
-  {
-    key = '-',
-    mods = 'ALT',
-    action = act.SplitVertical({domain = "CurrentPaneDomain"}),
-  },
-  {
-    key = 'h',
-    mods = 'ALT',
-    action = act.ActivatePaneDirection("Left"),
-  },
-  {
-    key = 'j',
-    mods = 'ALT',
-    action =  act.ActivatePaneDirection("Down"),
-  },
-  {
-    key = 'k',
-    mods = 'ALT',
-    action = act.ActivatePaneDirection("Up"),
-  },
-  {
-    key = 'l',
-    mods = 'ALT',
-    action = act.ActivatePaneDirection("Right"),
-  },
-  {
-    key = 'n',
-    mods = 'ALT',
-    action = act.SpawnTab("CurrentPaneDomain"),
-  },
-  {
-    key = 'f',
-    mods = 'ALT',
-    action = act.TogglePaneZoomState,
-  },
-  {
-    key = '/',
-    mods = 'ALT',
-    action = act.Search({CaseInSensitiveString = ""}),
-  },
+	{
+		key = "\\",
+		mods = "ALT",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "-",
+		mods = "ALT",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "ALT",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "j",
+		mods = "ALT",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "ALT",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "l",
+		mods = "ALT",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "n",
+		mods = "ALT",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "f",
+		mods = "ALT",
+		action = act.TogglePaneZoomState,
+	},
+	{
+		key = "/",
+		mods = "ALT",
+		action = act.Search({ CaseInSensitiveString = "" }),
+	},
+	{
+		key = "Enter",
+		mods = "ALT",
+		action = "DisableDefaultAssignment",
+	},
 }
 
 local map = function(key, mods, action)
-  if type(mods) == "string" then
-    table.insert(c.keys, { key = key, mods = mods, action = action })
-  elseif type(mods) == "table" then
-    for _, mod in pairs(mods) do
-      table.insert(c.keys, { key = key, mods = mod, action = action })
-    end
-  end
+	if type(mods) == "string" then
+		table.insert(c.keys, { key = key, mods = mods, action = action })
+	elseif type(mods) == "table" then
+		for _, mod in pairs(mods) do
+			table.insert(c.keys, { key = key, mods = mod, action = action })
+		end
+	end
 end
 
 for i = 1, 9 do
-  map(tostring(i), { "ALT" }, act.ActivateTab(i - 1))
+	map(tostring(i), { "ALT" }, act.ActivateTab(i - 1))
 end
 
 return c
