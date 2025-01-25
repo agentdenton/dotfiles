fbc5d1d9fadc16f87c8be7f8c06fd63881b39997
Andy Brychenko <andybrychenko@gmail.com>
2025-01-25 12:56:32 +0200

    nvim: Split configuration into profiles. Switch for helix for the time being

--- nvim/profiles/lazynvim/config/autocmds.lua
+++ nvim/profiles/lazynvim/config/autocmds.lua
@@ -5,32 +5,41 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
+-- highlight on yank
+augroup('YankHighlight', {clear = true})
+autocmd('TextYankPost', {
+  group = 'YankHighlight',
+  callback = function()
+    vim.highlight.on_yank({higroup = 'IncSearch', timeout = '100'})
+  end
+})
+
+-- remove whitespace on save
+autocmd('BufWritePre', {
+  pattern = '*.{not md}',
+  command = ":%s/\\s\\+$//e"
+})
+
 -- don't auto commenting new lines
 autocmd('BufEnter', {
   pattern = '*',
   command = 'set fo-=c fo-=r fo-=o'
 })
 
--- don't auto commenting new lines
-autocmd("BufEnter", {
-  pattern = "*",
-  command = "set fo-=c fo-=r fo-=o",
-})
-
 -- set indentation to 4 spaces
-augroup("setIndent4", { clear = true })
-autocmd("Filetype", {
-  group = "setIndent4",
-  pattern = { "python", "rust" },
-  command = "set softtabstop=4 shiftwidth=4 tabstop=4",
+augroup('setIndent4', {clear = true})
+autocmd('Filetype', {
+  group = 'setIndent4',
+  pattern = {'python', 'rust'},
+  command = 'set softtabstop=4 shiftwidth=4 tabstop=4'
 })
 
 -- set indentation to 2 spaces
-augroup("setIndent2", { clear = true })
-autocmd("Filetype", {
-  group = "setIndent2",
-  pattern = { "lua", "cpp", "c" },
-  command = "set softtabstop=2 shiftwidth=2 tabstop=2",
+augroup('setIndent2', {clear = true})
+autocmd('Filetype', {
+  group = 'setIndent2',
+  pattern = {'lua', 'cpp', 'c'},
+  command = 'set softtabstop=2 shiftwidth=2 tabstop=2'
 })
 
 -- remove line numbers in terminal mode
@@ -50,10 +59,10 @@ autocmd('BufLeave', {
   command = 'stopinsert'
 })
 
-if vim.fn.executable('rustfmt') == 1 then
-  -- run rustfmt on save
-  autocmd("BufWrite", {
-    pattern = "*.rs",
-    command = 'execute "!rustfmt \'%\'" | edit | silent',
-  })
-end
+-- if vim.fn.executable('rustfmt') == 1 then
+--   -- run rustfmt on save
+--   autocmd("BufWrite", {
+--     pattern = "*.rs",
+--     command = 'execute "!rustfmt \'%\'" | edit | silent',
+--   })
+-- end
