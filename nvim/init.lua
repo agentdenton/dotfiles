41b0b76d67f89c50a419f8412f5b1677428797ab
Andy Brychenko <andybrychenko@gmail.com>
2025-07-13 08:59:32 +0300

    nvim: Transparent background

--- nvim/init.lua
+++ nvim/init.lua
@@ -695,6 +695,7 @@ require("lazy").setup({
 		priority = 1000,
 		config = function()
 			require("catppuccin").setup({
+				transparent_background = true,
 				no_italic = true,
 			})
 			vim.cmd.colorscheme("catppuccin-frappe")
