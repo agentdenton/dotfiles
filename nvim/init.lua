eab47702de74bceb856736a6fab2eaf53ff90de7
Andy Brychenko <andybrychenko@gmail.com>
2025-07-14 15:08:23 +0300

    nvim: Enable line numbers

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,8 +8,8 @@ vim.g.maplocalleader = " "
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
-vim.opt.number = false
-vim.opt.relativenumber = false
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
