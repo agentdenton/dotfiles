4f7ef5a186488111c57327cc732d12932a8f9e52
Andy Brychenko <andybrychenko@gmail.com>
2025-07-29 07:14:41 +0300

    fish: Transparent fzf background

--- config.fish
+++ config.fish
@@ -36,7 +36,7 @@ set -x fish_pager_color_description 737994
 set -x FZF_DEFAULT_COMMAND "rg --files --no-ignore-vcs --hidden"
 set -x FZF_DEFAULT_OPTS "-m --height 50%"
 set -x FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS \
-    "--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284" \
+    "--color=bg+:#414559,bg:-1,spinner:#f2d5cf,hl:#e78284" \
     "--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf" \
     "--color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
 
