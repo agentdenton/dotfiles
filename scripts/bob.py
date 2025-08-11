df529c7b6ef6feba3e14951ae8292632ebdb4450
Andy Brychenko <andybrychenko@gmail.com>
2025-08-11 15:08:12 +0300

    bob: Visual tweaks

--- scripts/bob.py
+++ scripts/bob.py
@@ -7,6 +7,7 @@ import pathlib
 import argparse
 import subprocess
 
+
 def home_dir():
     """Gets the real user's home directory, even when run with sudo."""
     sudo_user = os.environ.get("SUDO_USER")
@@ -15,22 +16,24 @@ def home_dir():
     else:
         return pathlib.Path.home()
 
+
 DOTFILES_DIR = home_dir() / ".dotfiles"
 HOME = home_dir()
 
 LINKS = {
-    f"{DOTFILES_DIR}/bashrc":                   f"{HOME}/.bashrc",
-    f"{DOTFILES_DIR}/zshrc":                    f"{HOME}/.zshrc",
-    f"{DOTFILES_DIR}/vimrc":                    f"{HOME}/.vimrc",
-    f"{DOTFILES_DIR}/inputrc":                  f"{HOME}/.inputrc",
-    f"{DOTFILES_DIR}/gitconfig":                f"{HOME}/.gitconfig",
-    f"{DOTFILES_DIR}/config.fish":              f"{HOME}/.config/fish/config.fish",
-    f"{DOTFILES_DIR}/mimeapps.list":            f"{HOME}/.config/mimeapps.list",
-    f"{DOTFILES_DIR}/starship.toml":            f"{HOME}/.config/starship.toml",
-    f"{DOTFILES_DIR}/nvim/init.lua":            f"{HOME}/.config/nvim/init.lua",
-    f"{DOTFILES_DIR}/helix/config.toml":        f"{HOME}/.config/helix/config.toml",
-    f"{DOTFILES_DIR}/wezterm/wezterm.lua":      f"{HOME}/.config/wezterm/wezterm.lua",
+    f"{DOTFILES_DIR}/bashrc": f"{HOME}/.bashrc",
+    f"{DOTFILES_DIR}/zshrc": f"{HOME}/.zshrc",
+    f"{DOTFILES_DIR}/vimrc": f"{HOME}/.vimrc",
+    f"{DOTFILES_DIR}/inputrc": f"{HOME}/.inputrc",
+    f"{DOTFILES_DIR}/gitconfig": f"{HOME}/.gitconfig",
+    f"{DOTFILES_DIR}/config.fish": f"{HOME}/.config/fish/config.fish",
+    f"{DOTFILES_DIR}/mimeapps.list": f"{HOME}/.config/mimeapps.list",
+    f"{DOTFILES_DIR}/starship.toml": f"{HOME}/.config/starship.toml",
+    f"{DOTFILES_DIR}/helix": f"{HOME}/.config/helix",
+    f"{DOTFILES_DIR}/nvim": f"{HOME}/.config/nvim",
+    f"{DOTFILES_DIR}/wezterm/wezterm.lua": f"{HOME}/.config/wezterm/wezterm.lua",
 }
+
 
 def install_dots():
     for src, dest in LINKS.items():
@@ -42,6 +45,7 @@ def install_dots():
 
         print(f"Linking {src_path} -> {dest_path}")
         dest_path.symlink_to(src_path)
+
 
 def install_scripts():
     dest = "/usr/local/bin"
@@ -57,26 +61,29 @@ def install_scripts():
     except PermissionError:
         print(f"Error: Permission denied.")
 
+
 def install_pacman_packages():
     cfg_path = DOTFILES_DIR / "pacman" / "denton-lp.txt"
     command = ["pacman", "-S", "--needed", "--noconfirm", "-"]
     try:
-        with open(cfg_path, 'r') as f:
+        with open(cfg_path, "r") as f:
             subprocess.run(command, stdin=f, check=True, text=True)
     except subprocess.CalledProcessError as e:
         if "you cannot perform this operation unless you are root" in e.stderr:
             print(f"Error: Permission denied.")
 
+
 def install_flatpaks():
     cfg_path = DOTFILES_DIR / "flatpak" / "denton-lp.txt"
-    with open(cfg_path, 'r') as f:
+    with open(cfg_path, "r") as f:
         flatpak_ids = [line.strip() for line in f if line.strip()]
     command = ["flatpak", "install", "flathub", "--assumeyes"] + flatpak_ids
     try:
-        with open(cfg_path, 'r') as f:
+        with open(cfg_path, "r") as f:
             subprocess.run(command, check=True, text=True)
     except:
         print("Error. Failed to install flatpaks")
+
 
 def main():
     p = argparse.ArgumentParser(
@@ -84,10 +91,7 @@ def main():
         description="Utility to manage personal configurations",
     )
     sb = p.add_subparsers(dest="command", metavar="")
-    install = sb.add_parser(
-        "install",
-        help="Install one or more components."
-    )
+    install = sb.add_parser("install", help="Install one or more components.")
     install.add_argument(
         "component",
         nargs="+",
@@ -109,6 +113,7 @@ def main():
         if "flatpaks" in args.component:
             print("Installing flatpaks...")
             install_flatpaks()
+
 
 if __name__ == "__main__":
     main()
