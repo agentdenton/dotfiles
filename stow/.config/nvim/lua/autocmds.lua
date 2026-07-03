483b893cdb979e3bebcd39051dcd97001850d571
Andy Brychenko <andybrychenko@gmail.com>
2026-07-03 19:42:38 +0300

    nvim: Minimal configuration

--- stow/.config/nvim/lua/autocmds.lua
+++ stow/.config/nvim/lua/autocmds.lua
@@ -1,6 +1,4 @@
 -- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("highlight-yank-ag", { clear = true }),
@@ -22,29 +20,4 @@ vim.api.nvim_create_autocmd("TermOpen", {
 	desc = "Remove line numbers in terminal mode",
 	group = term_ag,
 	command = "setlocal listchars= nonumber norelativenumber",
-})
-
-local indent2_ag = vim.api.nvim_create_augroup("indent2-ag", { clear = true })
--- Set indentation to 2 spaces
-vim.api.nvim_create_autocmd("Filetype", {
-	desc = "Set indentation to 2 spaces",
-	group = indent2_ag,
-	pattern = { "lua", "cpp", "c" },
-	command = "set softtabstop=2 shiftwidth=2 tabstop=2",
-})
-local indent4_ag = vim.api.nvim_create_augroup("indent4-ag", { clear = true })
--- Set indentation to 4 spaces
-vim.api.nvim_create_autocmd("Filetype", {
-	desc = "Set indentation to 4 spaces",
-	group = indent4_ag,
-	pattern = { "python", "rust", "sh" },
-	command = "set softtabstop=4 shiftwidth=4 tabstop=4",
-})
-
--- Remove whitespace on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-	desc = "Remove whitespace on save",
-	pattern = "*.{not md}",
-	group = vim.api.nvim_create_augroup("remove-whitespace-ag", { clear = true }),
-	command = ":%s/\\s\\+$//e",
 })
--- stow/.config/nvim/lua/options.lua
+++ stow/.config/nvim/lua/options.lua
@@ -5,13 +5,13 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+-- Disable Nerd Font icons as requested
+vim.g.have_nerd_font = false
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Enable mouse mode, can be useful for resizing splits for example!
+-- Enable mouse mode
 vim.opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
@@ -20,15 +20,18 @@ vim.opt.showmode = false
 vim.opt.wrap = true
 
 -- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
 vim.schedule(function()
 	vim.opt.clipboard = "unnamedplus"
 end)
 
 -- Enable break indent
 vim.opt.breakindent = true
+
+-- Set default indentation to 2 spaces
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 
 -- Save undo history
 vim.opt.undofile = true
@@ -51,20 +54,16 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
--- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+-- vim.opt.list = true
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
-vim.opt.cursorline = false
+vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
 vim.opt.swapfile = false
-
 vim.opt.winborder = "rounded"
